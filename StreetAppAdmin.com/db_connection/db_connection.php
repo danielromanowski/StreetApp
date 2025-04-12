3a92c5137e52e53f20e4095121c158640ba9580f
danielromanowski <105395019+danielromanowski@users.noreply.github.com>
2025-04-11 22:48:56 -0400

    Update db_connection.php

--- StreetAppAdmin.com/db_connection/db_connection.php
+++ StreetAppAdmin.com/db_connection/db_connection.php
@@ -1,1 +1,12 @@
+<?php
 
+    $host = "localhost";
+    $port = "5432";
+    $dbname = "streetdreams";
+    $user = "postgres";
+    $password = "root";
+
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+
+
+?>
