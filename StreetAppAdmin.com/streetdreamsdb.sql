e8a0a81a0f6119acb6743b53890ebb8fda99225e
danielromanowski <105395019+danielromanowski@users.noreply.github.com>
2025-05-15 15:14:36 -0400

    Delete StreetAppAdmin.com directory

--- StreetAppAdmin.com/streetdreamsdb.sql
+++
@@ -1,9 +0,0 @@
-Drop database if exists streetdreams;
-
-CREATE DATABASE streetdreams;
-
-\c streetdreams;
-
-Drop table if exists locations;
-
-CREATE TABLE locations (id SERIAL PRIMARY KEY, location_name VARCHAR(255) NOT NULL, latitude DOUBLE PRECISION NOT NULL, longitude DOUBLE PRECISION NOT NULL, radius DOUBLE PRECISION NOT NULL, performance_type VARCHAR(255) NOT NULL, duration INTEGER NOT NULL, reservation_type VARCHAR(255) NOT NULL);
